ea21e64dec00fbb1a625e63f23adf1c71a83749c
nicoopenna <npenna@frba.utn.edu.ar>
2024-05-25 16:52:51 +0000

    Paquete con interfaz y unidades

--- cpu/src/instrucciones.h
+++ cpu/src/instrucciones.h
@@ -8,7 +8,7 @@ void execute_set(t_pcb *pcb, char *registro, uint32_t valor);
 void execute_sum(t_pcb *pcb, char *registro_destino, char *registro_origen);
 void execute_sub(t_pcb *pcb, char *registro_destino, char *registro_origen);
 void execute_jnz(t_pcb *pcb, char *registro, uint32_t nuevo_program_counter);
-void execute_io_gen_sleep(t_pcb *pcb, char *interfaz, uint32_t unidades_de_trabajo);
+void execute_io_gen_sleep(t_pcb *pcb, char *nombre_interfaz, uint32_t unidades_de_trabajo);
 void execute_exit(t_pcb *pcb);
 
 
